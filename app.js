7d9196f1bc8ae56b09977f0f14d44118a8c7f5ec
kelvin <muhammadfalahalfarisi@gmail.com>
2023-07-13 07:45:31 +0800

    Saya membuat repo baru tentang basic node js

---
+++ app.js
@@ -0,0 +1,21 @@
+const validator = require("validator");
+
+//! Perlu di ingat bahwa ini adalah contoh salah satu pakcage di Npm yang bernama Validator
+// console.log( validator.isEmail('falahalfarisi@gmail.com'));
+// console.log(validator.isMobilePhone('098765','id-ID'));
+console.log(validator.isNumeric("098765"));
+
+//! Package Chalk untuk mendesign terminal
+const chalk = require("chalk");
+
+console.log(chalk.blueBright("Hello World"));
+console.log(chalk.greenBright(true));
+console.log(chalk.redBright(false));
+
+// const p = false;
+// const l = true;
+// console.log(p);
+// console.log(l);
+
+
+
